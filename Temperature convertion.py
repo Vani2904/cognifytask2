5a6cdfbbdb041b85286cda44c99fb32b15a14bb9
Vani2904 <142568607+Vani2904@users.noreply.github.com>
2023-10-03 22:05:20 +0530

    Add files via upload

---
+++ Temperature convertion.py
@@ -0,0 +1,11 @@
+celsius = float(input("Enter temperature \ in celsius: "))
+
+fahrenheint = (celsius * 1.8) + 32
+
+print(str(celsius )+" degree celsius is equal to " + str(fahrenheint )+ " degree fahrenheit.")
+
+fahrenheint = float(input("Enter temperature in fahrenheint: "))
+
+celsius = (fahrenheint -32)/1.8
+
+print(str(fahrenheint )+" degree of fahrenheint is equal to " + str(celsius )+ " degree celsius. ")
